372e0691835baf992e1ecf1189c5b3a6bc551c85
saeedsh78 <45710959+saeedsh78@users.noreply.github.com>
2024-05-13 00:11:54 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,6 @@ import os
 import json
 import requests
 from dotenv import load_dotenv, set_key
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-THEMOVIEDB_API_KEY = os.getenv("THEMOVIEDB_API_KEY")
-THEMOVIEDB_API_TOKEN = "Bearer " + os.getenv("THEMOVIEDB_API_TOKEN")
-THEMOVIEDB_SEARCH_LINK = "https://api.themoviedb.org/3/search/movie?query={}&year={}&api_key={}"
-THEMOVIEDB_SEARCH_LINK_TV = "https://api.themoviedb.org/3/search/tv?query={}&api_key={}"
-THEMOVIEDB_FIND_LINK = "https://api.themoviedb.org/3/find/{}?external_source=imdb_id&api_key={}"
-THEMOVIEDB_GET_INFO_LINK = "https://api.themoviedb.org/3/movie/{}?api_key={}"
-THEMOVIEDB_GET_INFO_LINK_TV = "https://api.themoviedb.org/3/tv/{}?api_key={}"
-THEMOVIEDB_GET_CAST_LINK = "https://api.themoviedb.org/3/movie/{}/credits?language=en-US"
-THEMOVIEDB_GET_CAST_LINK_TV = "https://api.themoviedb.org/3/tv/{}/credits?language=en-US"
-THEMOVIEDB_GET_EXTERNAL_ID_LINK_TV = "https://api.themoviedb.org/3/tv/{}/external_ids"
-THEMOVIEDB_API_DOWNLOAD_IMAGE_300 = "https://image.tmdb.org/t/p/w300{}"
-OMDB_API_KEY = os.getenv("OMDB_API_KEY")
-OMDB_GET_INFO_LINK = "http://www.omdbapi.com/?i={}&apikey={}"
 
 
 def api_test(type_ = "omdb"):
@@ -37,10 +21,32 @@ def api_test(type_ = "omdb"):
         return True
     return False
 
+
 def change_api(type_,api_key, api_token = None):
     if type_ == "omdb":
-        set_key(dotenv_path=dotenv_path, key_to_set="OMDB_API_KEY", value_to_set=api_key)
+        set_key(dotenv_path="./.env", key_to_set="OMDB_API_KEY", value_to_set=api_key)
     if type_ == "tmdb":
-        set_key(dotenv_path=dotenv_path, key_to_set="THEMOVIEDB_API_KEY", value_to_set=api_key)
-        set_key(dotenv_path=dotenv_path, key_to_set="THEMOVIEDB_API_TOKEN", value_to_set=api_token)
+        set_key(dotenv_path="./.env", key_to_set="THEMOVIEDB_API_KEY", value_to_set=api_key)
+        set_key(dotenv_path="./.env", key_to_set="THEMOVIEDB_API_TOKEN", value_to_set=api_token)
+
+
+if not os.path.exists("./.env"):
+    change_api("omdb", api_key="")
+    change_api("tmdb", api_key="", api_token="")
+
+load_dotenv()
+
+THEMOVIEDB_API_KEY = os.getenv("THEMOVIEDB_API_KEY")
+THEMOVIEDB_API_TOKEN = "Bearer " + os.getenv("THEMOVIEDB_API_TOKEN")
+THEMOVIEDB_SEARCH_LINK = "https://api.themoviedb.org/3/search/movie?query={}&year={}&api_key={}"
+THEMOVIEDB_SEARCH_LINK_TV = "https://api.themoviedb.org/3/search/tv?query={}&api_key={}"
+THEMOVIEDB_FIND_LINK = "https://api.themoviedb.org/3/find/{}?external_source=imdb_id&api_key={}"
+THEMOVIEDB_GET_INFO_LINK = "https://api.themoviedb.org/3/movie/{}?api_key={}"
+THEMOVIEDB_GET_INFO_LINK_TV = "https://api.themoviedb.org/3/tv/{}?api_key={}"
+THEMOVIEDB_GET_CAST_LINK = "https://api.themoviedb.org/3/movie/{}/credits?language=en-US"
+THEMOVIEDB_GET_CAST_LINK_TV = "https://api.themoviedb.org/3/tv/{}/credits?language=en-US"
+THEMOVIEDB_GET_EXTERNAL_ID_LINK_TV = "https://api.themoviedb.org/3/tv/{}/external_ids"
+THEMOVIEDB_API_DOWNLOAD_IMAGE_300 = "https://image.tmdb.org/t/p/w300{}"
+OMDB_API_KEY = os.getenv("OMDB_API_KEY")
+OMDB_GET_INFO_LINK = "http://www.omdbapi.com/?i={}&apikey={}"
 
